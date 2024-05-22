d44874f892ee11389db943b285f40fe81a829b55
clang0016 <130537679+clang0016@users.noreply.github.com>
2024-05-22 15:29:17 +0800

    insert form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,58 +19,95 @@
         <button onclick="doctorList()" class="nav">Doctor List</button>
     </nav>
 
-    <table id="patientTable" style="display: none">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Room & Floor</th>
-                <th>Date Admitted</th>
-                <th>Doctor</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($patients as $patient)
-                <tr>
-                    <td>{{$patient->firstName}} {{$patient->lastName}}</td>
-                    <td>{{$patient->status}}</td>
-                    <td>Room {{$patient->room}}, Floor {{$patient->floor}}</td>
-                    <td>{{$patient->dateAdmitted}}</td>
-                    <td>{{$patient->doctorName}}</td>
-                </tr>
-            @empty
-                
-            @endforelse
-        </tbody>
-    </table>
+    <div id="patientTable" style="display: none">
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Room & Floor</th>
+                        <th>Date Admitted</th>
+                        <th>Doctor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($patients as $patient)
+                        <tr>
+                            <td>{{$patient->firstName}} {{$patient->lastName}}</td>
+                            <td>{{$patient->status}}</td>
+                            <td>Room {{$patient->room}}, Floor {{$patient->floor}}</td>
+                            <td>{{$patient->dateAdmitted}}</td>
+                            <td>{{$patient->doctorName}}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No Patient currently.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        <div>
+            <h1>Admit New Patient:</h1>
+            <form method="post">
+                @csrf
+                <input type="text" name="firstName" id="firstName" placeholder="First Name">
+                <input type="text" name="lastName" id="lastName" placeholder="Last Name">
+                Status:
+                <select name="status" id="status">
+                    <option value="Admitted">Admitted</option>
+                    <option value="Discharged">Discharged</option>
+                    <option value="In Surgery">In Surgery</option>
+                    <option value="In Recovery">In Recovery</option>
+                    <option value="Critical Condition">Critical Condition</option>
+                    <option value="Stable">Stable</option>
+                </select>
+                <input type="number" name="roomNumber" id="roomNumber" placeholder="Room Number">
+                <input type="number" name="floorNumber" id="floorNumber" placeholder="Floor Number">
+                <input type="date" name="dateAdmitted" id="dateAdmitted">
+                <input type="text" name="doctorName" id="doctorName" placeholder="Doctor Name">
+                <input type="submit" value="Admit">
+            </form>
+        </div>
+    </div>
 
-    <table id="doctorTable" style="display: none">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Specialty</th>
-                <th>Floor</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($doctors as $doctor)
-                <tr>
-                    <td>{{$doctor->firstName}} {{$doctor->lastName}}</td>
-                    <td>{{$doctor->specialty}}</td>
-                    <td>{{$doctor->floor}}</td>
-                    <td>{{$doctor->status}}</td>
-                </tr>
-            @empty
-                
-            @endforelse
-        </tbody>
-    </table>
+    <div id="doctorTable" style="display: none">
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Specialty</th>
+                        <th>Floor</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($doctors as $doctor)
+                        <tr>
+                            <td>{{$doctor->firstName}} {{$doctor->lastName}}</td>
+                            <td>{{$doctor->specialty}}</td>
+                            <td>{{$doctor->floor}}</td>
+                            <td>{{$doctor->status}}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No Doctors Currently.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        <div>
+            
+        </div>
+    </div>
 
     <script src="{{asset('assets/js/js.js')}}"></script>
     <script>
         function view() {
-            window.location.href = "{{ route('view') }}";
+            window.location.href = " {{route('view')}} ";
         }
     </script>
 </body>
