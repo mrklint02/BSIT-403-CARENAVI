2aae5354cfe66b873f1d15ad75520051cba7722f
clang0016 <130537679+clang0016@users.noreply.github.com>
2024-06-13 23:46:34 +0800

    eeeee

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\DataController;
-use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\DataCollector\DataCollector;
-
-Route::get('view', [DataController::class, 'view'])->name('view');
-Route::get('/', [DataController::class, 'index'])->name('index');
-Route::post('/', [DataCollector::class, 'store']);
